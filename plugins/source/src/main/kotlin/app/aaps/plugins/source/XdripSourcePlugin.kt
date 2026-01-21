50227b3a3c2848591b4975e54307777d5e741f7f
AdLouwerse <36573217+AdLouwerse@users.noreply.github.com>
2026-01-21 12:54:37 +0100

    Update XdripSourcePlugin.kt
    Allow all xDrip sensors as advancedFiltering to make it possible to use FSL2 with calibration.
    
    override fun advancedFilteringSupported(): Boolean = true

--- plugins/source/src/main/kotlin/app/aaps/plugins/source/XdripSourcePlugin.kt
+++ plugins/source/src/main/kotlin/app/aaps/plugins/source/XdripSourcePlugin.kt
@@ -52,7 +52,9 @@ class XdripSourcePlugin @Inject constructor(
     var advancedFiltering = false
     override var sensorBatteryLevel = -1
 
-    override fun advancedFilteringSupported(): Boolean = advancedFiltering
+    // override fun advancedFilteringSupported(): Boolean = advancedFiltering
+    // Changed by Ad: Set advanced filtering to true for all xDrip sensors
+    override fun advancedFilteringSupported(): Boolean = true
 
     @VisibleForTesting
     fun detectSource(glucoseValue: GV) {
