d87accf4a95ceae7b2a24ac082038a424264ecda
AdLouwerse <36573217+AdLouwerse@users.noreply.github.com>
2026-05-30 20:19:24 +0200

    Update XdripSourcePlugin.kt
    another attempt to get advanced filtering working for all xDrip sources

--- plugins/source/src/main/kotlin/app/aaps/plugins/source/XdripSourcePlugin.kt
+++ plugins/source/src/main/kotlin/app/aaps/plugins/source/XdripSourcePlugin.kt
@@ -70,8 +70,6 @@ class XdripSourcePlugin @Inject constructor(
             SourceSensor.LIBRE_3,
         ).any { it == glucoseValue.sourceSensor }
         advancedFiltering = true
-    } else {
-        advancedFiltering = true
     }
 
     // cannot be inner class because of needed injection
